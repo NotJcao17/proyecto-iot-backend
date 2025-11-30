e40fafa013fd760ecadf66e778b3d77456965761
NotJcao17 <orozcojuanc27@gmail.com>
2025-11-30 01:25:31 -0600

    Configuración inicial del proyecto

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config(); // Para las variables env
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middlewares
+app.use(cors()); // Para conexiones externas
+app.use(express.json()); // Para que el server entienda JSON
+
+// Ruta de prueba
+app.get('/', (req, res) => {
+  res.send('Al menos el servidor funciona');
+});
+
+// Iniciar servidor
+app.listen(PORT, () => {
+  console.log(`Servidor funcionando en http://localhost:${PORT}`);
+});
